1fa7a0d22bf58ee60fd0e46796db57f5c141ccbc
Marco Calamo (IlKaiser) <samuele.rinaldi90@gmail.com>
2021-09-29 22:06:09 +0200

    Update Loging

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,16 @@
 <template>
    <v-app>
-      <v-content >
+      <v-main >
          <v-container fluid fill-height>
             <v-layout align-center justify-center>
                <v-flex xs12 sm8 md4>
                   <v-card class="elevation-12">
-                     <v-toolbar dark color="secondary">
+                     <v-toolbar dark color="primary">
                         <v-spacer />
                         <v-toolbar-title>Login</v-toolbar-title>
                         <v-spacer />
                      </v-toolbar>
+                     
                      <v-card-text>
                         <v-form>
                            <v-text-field
@@ -22,6 +23,7 @@
                               rounded
                               dense
                               required
+                             
                            ></v-text-field>
                            <v-text-field
                               id="password"
@@ -33,18 +35,39 @@
                               rounded
                               dense
                               required
+                             
                            ></v-text-field>
                         </v-form>
                      </v-card-text>
+
                      <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="primary" to="/">Accedi</v-btn>
+                        <v-btn color="secondary" to="/home">Accedi</v-btn>
                      </v-card-actions>
+
+                     <v-fab-transition>
+                        <v-btn  
+                        color="secondary" 
+                        fab 
+                        absolute
+                        small 
+                        dark 
+                        bottom 
+                        left 
+                        class="v-btn--example"
+                        @click="$vuetify.theme.dark=!$vuetify.theme.dark">
+                           <v-icon>{{ activeFab.icon }}</v-icon>
+                        </v-btn>
+                     </v-fab-transition>
+
                   </v-card>
+                  
                </v-flex>
+              
             </v-layout>
+         
          </v-container>
-      </v-content>
+      </v-main>
    </v-app>
 </template>
 
@@ -56,7 +79,16 @@ export default {
       emailRules: [ 
         v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Email non valida.'
       ],
-    })
+    }),
+   computed: {
+      activeFab () {
+         if(!this.$vuetify.theme.dark) {
+         return { color: 'success', icon: 'mdi-moon-waning-crescent' }      
+         }else{
+         return{ color: 'success', icon: 'mdi-white-balance-sunny'}
+         }
+      },
+   },
 };
 </script>
 
