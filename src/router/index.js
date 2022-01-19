bfdfa603c97663a6823daa0ca6e5dd3b9479ddc6
Marco Calamo (IlKaiser) <samuele.rinaldi90@gmail.com>
2022-01-19 22:11:57 +0100

    Cleanup 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
-import Clienti from '../views/Clienti.vue'
-import Fabbriche from '../views/Fabbriche.vue'
-import Fustelle from '../views/Fustelle.vue'
-import Fustella_post from '../views/Fustella_post.vue'
-import Fustella_live from '../views/Fustella_live.vue'
+
 import Fustella_live_dash from '../views/Fustella_live_dash.vue'
 import PageNotFound from '../views/PageNotFound.vue'
 import WaitRoom from '../views/WaitRoom.vue'
@@ -19,36 +15,6 @@ const routes = [
     path: '/',
     name: 'login',
     component: Login
-  },
-  {
-    path: '/clienti',
-    name: 'clienti',
-    component: Clienti
-  },
-  {
-    path: '/fabbriche',
-    name: 'fabbriche',
-    component: Fabbriche
-  },
-  {
-    path: '/fustelle/c/:id',
-    name: 'fustelle_c',
-    component: Fustelle
-  },
-  {
-    path: '/fustelle/f/:id',
-    name: 'fustelle_f',
-    component: Fustelle
-  },
-  {
-    path: '/fustella/storico/:id',
-    name: 'fustella_post',
-    component: Fustella_post
-  },
-  {
-    path: '/fustella/live/:id',
-    name: 'fustella_live',
-    component: Fustella_live
   },
   {
     path: '/fustella/live/dash/:id',
